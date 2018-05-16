98ad7db85252cdb35b92f9f5342d8b30775e4b4d
luoshida <791371894@qq.com>
2018-05-16 21:07:00 +0800

    add

--- project/mi/js/font.js
+++ project/mi/js/font.js
@@ -1,25 +1,65 @@
 window.onload=function(){
+	handleCart();
+	handleNav();
+	handleMenu()
+}
+function handleCart(){
 	var tNav=document.querySelector('.nav');
 	var tNavD=document.querySelector('.nav .navD');
 	var tNavDS=tNavD.firstElementChild;
 	var tNavI=tNav.firstElementChild;
 	var tNavA=tNavI.nextElementSibling;
-	console.log(tNav);
 	tNav.onmouseenter=function(){
 		tNav.style.backgroundColor='#fff';
 		tNavI.style.color='red';
 		tNavA.style.color='red';
 		
-		animation(tNavD,{'height':300},true,function(){
-			tNavD.style.borderTop='1px solid #b0b0b0'
-		})
+		animation(tNavD,{'height':100},true,)
 	}
 	tNav.onmouseleave=function(){
 		tNav.style.backgroundColor='#424242';
 		tNavI.style.color='#b0b0b0';
 		tNavA.style.color='#b0b0b0';
-		animation(tNavD,{height:'0px'},true,function(){
-			tNavD.style.borderTop='none'
-		})
+		animation(tNavD,{'height':0},true)
 	}
+}
+function handleNav(){
+	var uBox1=document.querySelector('.box1'); 
+	var uBox1Li=uBox1.getElementsByTagName('li');
+	var uNavcontent=document.querySelector('.nav-content');
+	var timer;
+	for (var i = 0; i < uBox1Li.length; i++) {
+		uBox1Li[i].onmouseenter=function(){
+			clearTimeout(timer);
+			animation(uNavcontent,{'height':200})
+		} 
+		uBox1Li[i].onmouseleave=function(){
+			timer=setTimeout(function(){
+				animation(uNavcontent,{'height':0},true)
+			},300);
+		}
+		uNavcontent.onmouseenter=function(){
+			clearTimeout(timer);
+		}
+		uNavcontent.onmouseleave=function(){
+			timer=setTimeout(function(){
+				animation(uNavcontent,{'height':0},true)
+			},300);
+		}
+	}
+}
+function handleMenu(){
+	var uBox2=document.querySelector('.up .box2');
+	var uBox2D=document.querySelector('.box2 .box2D');
+	var uBox2I=document.querySelector('.box2 .box2I');
+	uBox2I.onfocus=function(){
+		uBox2D.style.display='block';
+		uBox2I.style.outline='none';
+		uBox2I.style.borderColor='orange';
+	}
+	uBox2I.onblur=function(){
+		uBox2D.style.display='none';
+		uBox2I.style.borderColor='#e0e0e0';
+	}
+
 }
